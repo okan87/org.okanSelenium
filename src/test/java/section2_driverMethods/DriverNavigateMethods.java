4ebc077f23b8d634edb0e352aa5b6c05e23d6531
okan87 <okankaplan1907@gmail.com>
2025-04-14 18:56:42 +0200

    driver.navigateMethods added

--- src/test/java/section2_driverMethods/DriverNavigateMethods.java
+++ src/test/java/section2_driverMethods/DriverNavigateMethods.java
@@ -1,4 +1,36 @@
 package section2_driverMethods;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverNavigateMethods {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+//       1-  driver.navigate().to("https://www.facebook.com")
+        driver.get("https://www.amazon.com");
+        driver.navigate().to("https://www.facebook.com");
+
+//        2- driver.navigate().back()
+        driver.navigate().back();
+//        3- driver.navigate().forward()
+        driver.navigate().forward();
+//       4- driver.navigate().refresh();
+        driver.navigate().refresh();
+
+        //for page
+//        driver.close();
+
+        //for all tabs
+        driver.quit();
+
+
+
+
+
+
+
+    }
 }
