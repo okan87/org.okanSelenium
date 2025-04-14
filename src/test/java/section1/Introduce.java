b91a089ddc47777e306e59c033b8f0c1fe8ed203
okan87 <okankaplan1907@gmail.com>
2025-04-14 18:16:11 +0200

    driver.get added

--- src/test/java/section1/Introduce.java
+++ src/test/java/section1/Introduce.java
@@ -1,7 +1,14 @@
 package section1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Introduce {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.nl");
     }
 }
